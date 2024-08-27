877af726beb0ef85fb5c69a6a6cf101efba624d0
Swapnik Saiba <57573646+swapniksaiba@users.noreply.github.com>
2024-08-27 22:56:24 +0530

    Delete sample.py

--- sample.py
+++
@@ -1,8 +0,0 @@
-def calculate_area(radius):
-    pi = 3.14
-    area = pi * radius ** 2
-    return area
-
-# Function call with a typo in the function name
-result = calculate_are(5)
-print("Area:", result)
